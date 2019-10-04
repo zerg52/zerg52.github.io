8063e52385cf6d49f25a4a3740c6c1969c3aa96a
zerg52 <zerg52@users.noreply.github.com>
2019-10-04 14:44:03 +0300

    e

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+  var width = window.innerWidth;
+  var height = window.innrerHeight;
+  var canvas = document.getElementById('canvas');
+
+  canvas.setAttribute('width', width);
+  canvas.setAttribute('height', height);
+
+  var renderer = new THREE.WebGLRenderer({canvas:canvas});
+  renderer.setClearColor(0x000000);
+
+  var scene = new THREE.Scene();
+  var camer = new THREE.PerspectiveCamera();
+}
