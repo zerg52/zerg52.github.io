a618181ea0fd728c751a3e29bb5dfb2f1e320d31
zerg52 <zerg52@users.noreply.github.com>
2019-10-04 16:07:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,31 @@
-/////////////////////////////////////////
-// Scene Setup
-/////////////////////////////////////////
+window.onload = function(){
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var canvas = document.getElementById('canvas');
 
-var scene,
-    camera,
-    renderer,
-    controls;
+  canvas.setAttribute('width', width);
+  canvas.setAttribute('height', height);
 
-scene = new THREE.Scene();
+  var renderer = new THREE.WebGLRenderer({canvas:canvas});
+  renderer.setClearColor(0x000000);
 
-camera = new THREE.PerspectiveCamera( 10, window.innerWidth / window.innerHeight, 1, 1000 );
-camera.position.set(-5, 12, 10);
-camera.lookAt( scene.position );
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(60,width/height,0.1,5000);
 
-renderer = new THREE.WebGLRenderer({
-  alpha: true,
-	antialias: true
-});
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
+  camera.position.set (0,0,1500);
 
-document.body.appendChild( renderer.domElement );
+  var light = new THREE.AmbientLight(0xffffff);
+  scene.add(light);
 
+  var geometry = new THREE.SphereGeometry(300,10,10);
+  var material = new THREE.MeshBasicMaterial({color: 0x00ff00,wireframe: true});
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-/////////////////////////////////////////
-// Trackball Controller
-/////////////////////////////////////////
-
-controls = new THREE.TrackballControls( camera );
-controls.rotateSpeed = 5.0;
-controls.zoomSpeed = 3.2;
-controls.panSpeed = 0.8;
-controls.noZoom = false;
-controls.noPan = true;
-controls.staticMoving = false;
-controls.dynamicDampingFactor = 0.2;
-
-
-/////////////////////////////////////////
-// Lighting
-/////////////////////////////////////////
-
-var iphone_color  = '#FAFAFA',
-    ambientLight  = new THREE.AmbientLight( '#EEEEEE' ),
-    hemiLight     = new THREE.HemisphereLight( iphone_color, iphone_color, 0 ),
-    light         = new THREE.PointLight( iphone_color, 1, 100 );
-
-hemiLight.position.set( 0, 50, 0 );
-light.position.set( 0, 20, 10 );
-
-scene.add( ambientLight );
-scene.add( hemiLight );
-scene.add( light );
-
-
-/////////////////////////////////////////
-// Utilities
-/////////////////////////////////////////
-
-var axisHelper = new THREE.AxisHelper( 1.25 );
-scene.add( axisHelper );
-
-
-/////////////////////////////////////////
-// Render Loop
-/////////////////////////////////////////
-
-function renderPhone() {
-  renderer.render( scene, camera );
+function loop(){
+mesh.rotation.y += Math.PI/500;
+  renderer.render(scene, camera);
+	requestAnimationFrame(function(){loop();});
+	}
+	loop();
 }
-
-// Render the scene when the controls have changed.
-// If you don’t have other animations or changes in your scene,
-// you won’t be draining system resources every frame to render a scene.
-controls.addEventListener( 'change', renderPhone );
-
-// Avoid constantly rendering the scene by only
-// updating the controls every requestAnimationFrame
-function animationLoop() {
-  requestAnimationFrame(animationLoop);
-  controls.update();
-}
-
-animationLoop();
-
-
-/////////////////////////////////////////
-// Window Resizing
-/////////////////////////////////////////
-
-window.addEventListener( 'resize', function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-	controls.handleResize();
-	renderPhone();
-}, false );
-
-
-/////////////////////////////////////////
-// Object Loader
-/////////////////////////////////////////
-
-var dae,
-    loader = new THREE.ColladaLoader();
-
-function loadCollada( collada ) {
-  dae = collada.scene;
-  dae.position.set(0.4, 0, 0.8);
-  scene.add(dae);
-  renderPhone();
-}
-
-loader.options.convertUpAxis = true;
-loader.load( '/antin.dae', loadCollada);
